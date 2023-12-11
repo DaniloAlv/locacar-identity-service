c8aa2cc0d2f94580248487f51f4171651b668d88
Danilo Alves <danilo_alves201090@hotmail.com>
2023-12-11 00:06:01 -0300

    remoção de diretório Utils não utilizado

--- LocacaoDeCarros.Identity/LocacaoDeCarros.Identity.API/Utils/TableNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LocacaoDeCarros.Identity.API.Utils
-{
-    public static class TableNames
-    {
-        public const string Role = "RoleIdentity";
-        public const string User = "UserIdentity";
-    }
-}
